026a6d94470f7e0cee42770b2fac6e66af3fd202
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-03-29 19:26:45 -0300

    feat: add data to SectionAboutUs

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,28 +1,26 @@
-export type LogoProps = {
+export type Image = {
   alternativeText: string
   url: string
 }
 
+export type Button = {
+  label: string
+  url: string
+}
+
+export type LogoProps = Image
+
 export type HeaderProps = {
   title: string
   description: string
-  button: {
-    label: string
-    url: string
-  }
-  image: {
-    alternativeText: string
-    url: string
-  }
+  button: Button
+  image: Image
 }
 
 export type SectionAboutProjectProps = {
   title: string
   description: string
-  image: {
-    alternativeText: string
-    url: string
-  }
+  image: Image
 }
 
 export type TechIcon = {
@@ -65,10 +63,25 @@ export type PricingBoxProps = {
   numberInstallments: number
   priceInstallment: number
   benefits: string
-  button: {
-    label: string
-    url: string
-  }
+  button: Button
+}
+
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
 }
 
 export type LandingPageProps = {
@@ -80,4 +93,5 @@ export type LandingPageProps = {
   sectionModules: SectionModulesProps
   sectionAgenda: SectionAgendaProps
   pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
 }
